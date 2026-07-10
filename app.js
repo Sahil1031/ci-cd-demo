41b93c7d3238bbd419233ef5e69263d730d9f2e1
SAHIL1031 <bagdesahil31@gmail.com>
2026-07-10 15:52:50 +0530

    Testing full automation

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const http = require('http');
 
 const server = http.createServer((req, res) => {
     res.writeHead(200, {'Content-Type': 'text/plain'});
-    res.end('Hello Sahil! CI/CD Pipeline is Working Perfectly ! 🚀\n');
+    res.end('Hello Sahil! fully Automated NO manual SSH needed 🚀\n');
 });
 
 server.listen(3000, () => {
